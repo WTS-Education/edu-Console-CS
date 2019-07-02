4a4d20b96643d46282f44d3be407d418c76b0f60
WTS-DESKTOP06\nishitani.yuuki <nishitani.yuuki@wintechservice.co.jp>
2019-07-02 09:41:49 +0900

    課題1

--- Task1_birthdayCalculation/Task1_birthdayCalculation/BirthdayCalculation.cs
+++ Task1_birthdayCalculation/Task1_birthdayCalculation/BirthdayCalculation.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+class Birthdaycalculation
 
-namespace Task1_birthdayCalculation
 {
-    class BirthdayCalculation
+    public static void Main()
     {
-        static void Main(string[] args)
+        Console.WriteLine("生年月日を入力してください。");
+
+
+        var seinengappi = new DateTime(Console.Read());
+        var today = System.DateTime.Today;
+
+        if (seinengappi = ())
         {
-            Console.WriteLine("Hello World!");
+
         }
+
+        TimeSpan elapsedSpan = today - seinengappi;
+
+        Console.WriteLine("今日はあなたが生まれてから{0}か月目です。", elapsedSpan.TotalDays / 12);
+        Console.WriteLine("今日はあなたが生まれてから{0}日目です。", elapsedSpan.TotalDays);
+        Console.WriteLine("現時刻はあなたが生まれてから{0}時間です。", elapsedSpan.TotalHours);
+        Console.WriteLine("現時刻はあなたが生まれてから{0}秒です。", elapsedSpan.TotalMinutes);
+
+
     }
 }
