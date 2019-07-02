aaad12f107ce1fc23d49a2f811e6974f6ba708d1
WTS-DESKTOP06\nishitani.yuuki <nishitani.yuuki@wintechservice.co.jp>
2019-07-02 11:39:46 +0900

    Revert "課題1"
    This reverts commit 4a4d20b96643d46282f44d3be407d418c76b0f60.

--- Task1_birthdayCalculation/Task1_birthdayCalculation/BirthdayCalculation.cs
+++ Task1_birthdayCalculation/Task1_birthdayCalculation/BirthdayCalculation.cs
@@ -1,27 +1,12 @@
 ﻿using System;
-class Birthdaycalculation
 
+namespace Task1_birthdayCalculation
 {
-    public static void Main()
+    class BirthdayCalculation
     {
-        Console.WriteLine("生年月日を入力してください。");
-
-
-        var seinengappi = new DateTime(Console.Read());
-        var today = System.DateTime.Today;
-
-        if (seinengappi = ())
+        static void Main(string[] args)
         {
-
+            Console.WriteLine("Hello World!");
         }
-
-        TimeSpan elapsedSpan = today - seinengappi;
-
-        Console.WriteLine("今日はあなたが生まれてから{0}か月目です。", elapsedSpan.TotalDays / 12);
-        Console.WriteLine("今日はあなたが生まれてから{0}日目です。", elapsedSpan.TotalDays);
-        Console.WriteLine("現時刻はあなたが生まれてから{0}時間です。", elapsedSpan.TotalHours);
-        Console.WriteLine("現時刻はあなたが生まれてから{0}秒です。", elapsedSpan.TotalMinutes);
-
-
     }
 }
